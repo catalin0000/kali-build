03f1e6b311ab24a14fef4ea9db6c79fdb0a4a48e
doesntmatter <doesntmatter>
2024-07-06 22:01:52 +0100

    whatever

--- install.sh
+++ install.sh
@@ -28,19 +28,6 @@ sed -i "s|$search_string|$new_line|" "$file"
 oh-my-posh font install hack
 oh-my-posh font install AnonymousPro
 
-search_string='fontFamily='
-new_line='fontFamily=Hack Nerd Font'
-sed -i "s|^${search_string}.*|${new_line}|" "$file"
-
-search_string='fontSize='
-new_line='fontSize=12'
-sed -i "s|^${search_string}.*|${new_line}|" "$file"
-
-search_string='ApplicationTransparency='
-new_line='ApplicationTransparency=0'
-sed -i "s|^${search_string}.*|${new_line}|" "$file"
-
-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
@@ -49,7 +36,7 @@ line='eval "$(oh-my-posh init zsh --config /home/kali/.config/oh-my-posh/themes/
 echo "$line" >> "$file"
 
 apt update
-apt install zaproxy nuclei testssl.sh sublime-text kate feroxbuster wireguard mosh oscanner tnscmd10g wkhtmltopdf
+apt install -y zaproxy nuclei testssl.sh sublime-text kate feroxbuster wireguard mosh oscanner tnscmd10g wkhtmltopdf
 
 mkdir /mnt/hgfs
 file="/etc/fstab"
@@ -73,5 +60,17 @@ git clone https://github.com/Tib3rius/AutoRecon
 git clone https://github.com/Ekultek/WhatWaf
 git clone https://github.com/threat9/routersploit
 
+file='/home/kali/.config/qterminal.org/qterminal.ini
+search_string='fontFamily='
+new_line='fontFamily=Hack Nerd Font'
+sed -i "s|^${search_string}.*|${new_line}|" "$file"
 
+search_string='fontSize='
+new_line='fontSize=12'
+sed -i "s|^${search_string}.*|${new_line}|" "$file"
 
+search_string='ApplicationTransparency='
+new_line='ApplicationTransparency=0'
+sed -i "s|^${search_string}.*|${new_line}|" "$file"
+
+qterminal &
